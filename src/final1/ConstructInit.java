76c92d023e52e2b36278d0d3fbad626a1326ff42
kdhdd <109668066+kdhdd@users.noreply.github.com>
2024-01-10 22:32:30 +0900

    2024-01-10

---
+++ src/final1/ConstructInit.java
@@ -0,0 +1,11 @@
+package final1;
+
+public class ConstructInit {
+
+    final int value;
+
+    //생성자를 통해 한 번 할당 가능
+    public ConstructInit(int value) {
+        this.value = value;
+    }
+}
---
+++ src/final1/FieldInit.java
@@ -0,0 +1,12 @@
+package final1;
+
+public class FieldInit {
+
+    static final int CONST_VALUE = 10;
+    final int value = 10;
+
+    //public FieldInit(int value) {
+        //이미 할당 된 값이 있으므로 불가능
+        //this.value = value;
+
+}
